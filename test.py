6b04f129d32bf18ce047a2087fbc9c70ee5cd8dd
Juan E. Sanchez <juan@tcad.com>
2021-04-20 23:08:37 -0500

    more work

--- test.py
+++ test.py
@@ -1,12 +1,23 @@
 
+# start with 1D FFT
 class hbconfig:
     def __init__(self):
-        pass
+        self._number_harmonics = None
+        self._fundamental = None
+        self._number_rows = None
+        self._initial_guess = None
+        # callback to set the bias
+        self._bias_callback = None
+        # callback to set the time dependent variable
+        self._variable_setter = None
+        # callback to aquire the time-dependent jacobian and rhs
+        self._matrix_rhs_getter = None
+
 
     # number of harmonics should be base 2 for most efficiency
     # frequencies
     # start with 1D 
-    def set_harmonics(number_harmonics):
+    def set_harmonics(self, number_harmonics):
         '''
         number_harmonics: Number of harmonics to consider
         '''
@@ -15,7 +26,13 @@ class hbconfig:
         # 0, 1, 2, 3, . . . nharm
         # how to make this into an fft number
 
+    def set_number_rows(self, number_rows):
+        self._number_rows = number_rows
+
+    def set_initial_guess(self, initial_guess):
+        pass
+
 
 if __name__ == '__main__':
-    hb = hbplan()
+    hb = hbconfig()
     hb.set_harmonics(5)
