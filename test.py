47883d303da788cb413c593e5a1f8516c46d2d2e
Juan E. Sanchez <juan@tcad.com>
2021-04-20 22:40:44 -0500

    another

--- test.py
+++ test.py
@@ -6,13 +6,16 @@ class hbconfig:
     # number of harmonics should be base 2 for most efficiency
     # frequencies
     # start with 1D 
-    def set_dimensions(number_harmonics):
+    def set_harmonics(number_harmonics):
         '''
         number_harmonics: Number of harmonics to consider
         '''
         self._number_harmonics = number_harmonics
+        # so this would be
+        # 0, 1, 2, 3, . . . nharm
+        # how to make this into an fft number
 
 
 if __name__ == '__main__':
     hb = hbplan()
-    print("done")
+    hb.set_harmonics(5)
