2daca3d849c366c7c75fe6ad040230350c617e69
Juan E. Sanchez <juan@tcad.com>
2021-04-24 00:59:59 -0500

    organize

--- test.py
+++ test.py
@@ -3,18 +3,25 @@ import test_circuit
 import numpy as np
 
 
-if __name__ == '__main__':
-    hb = hbconfig.hbconfig()
-    hb.set_harmonics(5)
 
-    circuit = test_circuit.circuit()
-    circuit.set_bias(0.5)
-    circuit.dc_solve()
-    sol = circuit.get_solution()
-    print(sol)
-    print((sol[1]-sol[2])/circuit.RS)
-    print(sol[0])
+# create circuit
+circuit = test_circuit.circuit()
 
-    hb.set_bias_callback(lambda x : circuit.set_bias(x))
-    hb.set_matrix_rhs_callback(lambda x : circuit.load_circuit())
-    hb.set_solution_callback(lambda x : circuit.set_solution(x))
+# set dc bias
+circuit.set_bias(0.5)
+
+# solve
+circuit.dc_solve()
+
+# get solution
+sol = circuit.get_solution()
+print(sol)
+print((sol[1]-sol[2])/circuit.RS)
+print(sol[0])
+
+# start with hb config
+hb = hbconfig.hbconfig()
+hb.set_harmonics(5)
+hb.set_bias_callback(lambda x : circuit.set_bias(x))
+hb.set_matrix_rhs_callback(lambda x : circuit.load_circuit())
+hb.set_solution_callback(lambda x : circuit.set_solution(x))
