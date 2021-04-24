aaec5346fd7d133373b73d9914d41eec6927a1cf
Juan E. Sanchez <juan@tcad.com>
2021-04-24 00:03:26 -0500

    more work

--- test.py
+++ test.py
@@ -10,12 +10,18 @@ if __name__ == '__main__':
 
     circuit = test_circuit.circuit()
     circuit.set_bias(0.5)
-    for i in range(1):
+    for i in range(5):
         j, f = circuit.load_circuit()
         #print(f)
         #print(linalg.inv(j))
         update = -np.dot(linalg.inv(j),f)
         print(update)
+        newsol = circuit.get_solution() + update
+        circuit.set_solution(newsol)
         #print(update.shape)
         #print(f.shape)
+    sol = circuit.get_solution()
+    print(sol)
+    print((sol[1]-sol[2])/circuit.RS)
+    print(sol[0])
 
